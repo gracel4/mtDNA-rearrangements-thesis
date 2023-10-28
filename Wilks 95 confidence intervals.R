25d1fd6ba81e17c3e47848bb6b4b211e9d0c3676
gracel4 <147788174+gracel4@users.noreply.github.com>
2023-10-29 09:43:30 +1100

    Add files via upload

---
+++ Wilks 95 confidence intervals.R
@@ -0,0 +1,16 @@
+library(reshape2)
+library(ggplot2)
+
+wilks<-read.csv("Ultra wilks range.csv")
+head(wilks)
+boxplot(wilks[,-1], ylab="Length of 95% Confidence Interval")
+
+median(my_data)
+median(unlist(my_data2))
+median(unlist(my_data3))
+median(unlist(my_data4))
+median(unlist(my_data5))
+median(unlist(my_data6))
+
+
+                      
